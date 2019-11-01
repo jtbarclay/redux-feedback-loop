f713639348f46bb61b8fe226e99fe2b5f4769501
Jon Barclay <my@nameisjon.com>
2019-11-01 16:13:59 -0500

    controlled input, button, and handlers for feelings component

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
 class Feeling extends Component {
+  state = {
+    feeling: '',
+  }
+
+  handleInput = (event) => {
+    this.setState({
+      feeling: event.target.value,
+    })
+  }
+  
+  handleButton = () => {
+
+    this.props.history.push('/understanding');
+  }
+
   render() {
     return (
       <>
-      Feeling
+      <h2>How are you feeling today?</h2>
+      <label>Feeling?</label>
+      <input onChange={this.handleInput} value={this.state.feeling}/>
+      <button onClick={this.handleButton}>Next</button>
+
+      
+      <br/>
+      <br/>
+      <pre>State:{JSON.stringify(this.state, null, 2)}</pre>
+      <br/>
+      <br/>
+      <pre>Props:{JSON.stringify(this.props, null, 2)}</pre>
       </>
     );
   }
